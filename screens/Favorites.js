1c76afa2142731bda820d21c2c6294d1622b098a
Wheeler-Christian <christianwheeler505@gmail.com>
2024-02-14 13:36:49 -0700

    Test branch feature/favorites-screen

--- screens/Favorites.js
+++ screens/Favorites.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text } from "react-native";
 
 function Favorites() {
-  return <Text style={styles.text}>The favorites screen</Text>;
+  return <Text style={styles.text}>The favorites screen. Start working on this.</Text>;
 }
 
 export default Favorites;
